6910ee72e14e44531e5feeaef2d0d9ca3f1ced89
arevalolance <lancearevalo2000@gmail.com>
2022-08-23 21:03:37 +0800

    add: resume page and navbar link

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,35 +1,47 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState } from 'react';
-import MobileMenu from '../MobileMenu';
-import useTime from '../../hooks/useTime';
-import { Icon } from '@iconify/react';
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import MobileMenu from "../MobileMenu";
+import { Icon } from "@iconify/react";
+import Clock from "react-live-clock";
 
 const Navbar = () => {
-  const time = useTime();
   const [open, setOpen] = useState<Boolean>(false);
+  const [loadClock, setLoadClock] = useState<Boolean>(false);
+
+  useEffect(() => {
+    setLoadClock(true);
+  }, [loadClock]);
 
   return (
     <>
-      <header className="w-full h-[100px] flex flex-row-reverse md:flex-row justify-between items-center sticky top-0 bg-[#0D0D0D] z-10">
-        <span className="label">{time}</span>
-
+      <header className="w-full max-w-[1280px] h-[100px] mx-auto flex flex-row-reverse md:flex-row justify-between items-center sticky top-0 z-10 transition-opacity delay-500">
+        <div className="hover:text-white hover:cursor-pointer">
+          {loadClock ? (
+            <Clock
+              className="text-sm font-space-grotesk"
+              format={"hh:mm:ss A"}
+              ticking={true}
+              timezone={"Asia/Manila"}
+            />
+          ) : null}
+          <span className="ml-2 font-space-grotesk text-sm">Luzon, PH </span>
+        </div>
         <nav className="hidden md:flex md:flex-row">
           <Link href="/">
-            <span className="label hover:cursor-pointer hover:underline">
+            <span className="text-sm font-space-grotesk hover:cursor-pointer hover:text-white hover:underline mr-5">
               home
             </span>
           </Link>
           {/* <Link href="/blogs">
-            <span className="label hover:cursor-pointer hover:underline">
+            <span className="text-sm font-space-grotesk hover:cursor-pointer hover:underline">
               blog
             </span>
-          </Link>
+          </Link>*/}
           <Link href="/resume">
-            <span className="label hover:cursor-pointer hover:underline">
+            <span className="text-sm font-space-grotesk hover:cursor-pointer hover:text-white hover:underline">
               resume
             </span>
-          </Link> */}
+          </Link>
         </nav>
 
         <button
@@ -39,7 +51,7 @@ const Navbar = () => {
           <Icon className="text-[35px]" icon="charm:menu-hamburger" />
         </button>
       </header>
-      <MobileMenu open={open} setOpen={setOpen} time={time} />
+      <MobileMenu open={open} setOpen={setOpen} />
     </>
   );
 };
---
+++ pages/resume.tsx
@@ -0,0 +1,136 @@
+import { ReactElement } from "react";
+import Layout from "../components/Layout";
+import { NextPageWithLayout } from "./_app";
+import ResumeRow, { ResumeRowType } from "../components/ResumeRow";
+import ResumeHeading from "../components/ResumeHeading";
+import Bullet from "../components/Bullet";
+
+const Resume: NextPageWithLayout = () => {
+  const tools = [
+    "JavaScript/TypeScript",
+    "Python",
+    "Go",
+    "Java",
+    "Git",
+    "Docker",
+    "Linux",
+    "SQL",
+  ];
+
+  const experience: ResumeRowType[] = [
+    {
+      role: "Business Applications Developer",
+      place: "(Remote) Makati, PH",
+      company: "Noah Business Applications",
+      date: "July - August, 2022",
+      description: [
+        {
+          job: "Quality Assurance",
+          description:
+            "Assured that each component's functionality in their system was up to date and withhold standards that is made by the company",
+        },
+      ],
+      listLabel: "Technologies and Tools",
+      list: ["Excel"],
+    },
+    {
+      role: "Student Developer",
+      place: "Baguio, PH",
+      company: "SLU Comelec",
+      date: "September 2020",
+      description: [
+        {
+          job: "Dashboard UI/UX Design",
+          description:
+            "Built and designed a dashboard page that displays system statistic and status for the administrator using HTML5 and CSS3",
+        },
+        {
+          job: "Quality Assurance",
+          description:
+            "Assured the robustness of the functionalities of the system through hands-on testing in different campuses",
+        },
+        {
+          job: "On-site Technical Support",
+          description:
+            "Implemented fair voting policies through on-site administration of system.",
+        },
+      ],
+      listLabel: "Technologies and Tools",
+      list: ["HTML5", "CSS3", "JavaScript"],
+    },
+    {
+      role: "On-the-job Training",
+      place: "Cabanatuan, PH",
+      company: "Cabanatuan City Police Station",
+      date: "November 2018",
+      description: [
+        {
+          job: "Database Management",
+          description:
+            "Managed and sorted Intel documents on different cases in the precinct.",
+        },
+        {
+          job: "Clerkship",
+          description:
+            "Handled clerkship for police clearances and led the team to produce efficient work.",
+        },
+        {
+          job: "Seminar",
+          description:
+            "Was introduced to the different procedures that the PNP does to manage crime in the city.",
+        },
+      ],
+      listLabel: "Technologies and Tools",
+      list: ["Excel", "Internal Applications in PNP"],
+    },
+  ];
+
+  return (
+    <>
+      <section className="mt-10 mb-20">
+        <div className="flex flex-row justify-between w-full">
+          <div className="flex flex-col text-left font-space-grotesk">
+            <span>Lance Arevalo</span>
+            <a
+              className="underline hover:text-white hover:cursor-pointer"
+              href="https://www.linkedin.com/in/arevalolance/"
+            >
+              contact me!
+            </a>
+          </div>
+          <div className="flex flex-col text-right font-space-grotesk">
+            <span>Full-Stack Developer</span>
+            <span>Luzon, Philippines</span>
+          </div>
+        </div>
+
+        <ResumeHeading>Tools and Technologies</ResumeHeading>
+        <ul className="flex flex-col flex-wrap max-h-[100px]">
+          {tools.map((item) => (
+            <Bullet key={item} string={item} icon={"akar-icons:gear"} />
+          ))}
+        </ul>
+
+        <ResumeHeading>Experience</ResumeHeading>
+        {experience.map((item) => (
+          <ResumeRow
+            key={item.company}
+            place={item.place}
+            role={item.role}
+            company={item.company}
+            date={item.date}
+            description={item.description}
+            listLabel={item.listLabel}
+            list={item.list}
+          />
+        ))}
+      </section>
+    </>
+  );
+};
+
+Resume.getLayout = function getLayout(page: ReactElement) {
+  return <Layout type={"others"}>{page}</Layout>;
+};
+
+export default Resume;
