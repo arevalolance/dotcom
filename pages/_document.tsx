f6c3c90dbbe942d5f8ac62f7bd6470747803a246
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-01-09 01:56:39 +0800

    Update meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,43 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+        <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link
+          href="/static/favicons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/static/favicons/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/static/favicons/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          color="#4a9885"
+          href="/static/favicons/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
+        <meta content="#171717" name="theme-color" />
+        <meta content="#171717" name="msapplication-TileColor" />
+        <meta
+          content="/static/favicons/browserconfig.xml"
+          name="msapplication-config"
+        />
+        <meta
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+          name="robots"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
