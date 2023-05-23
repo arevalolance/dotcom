350352e8bc7571d2e5a621c5b490a22bb140e164
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-05-23 20:18:02 +0800

    refactor: add color gradient to heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Balancer } from 'react-wrap-balancer';
-
 export default function Home() {
   return (
     <main className="container mx-auto">
@@ -8,10 +6,8 @@ export default function Home() {
           <div className="rounded-full mx-auto md:mx-0 bg-gray-100 hover:bg-gray-200 hover:duration-150 ease-in w-fit px-3 py-1 text-black font-semibold text-xs">
             Nice to meet you
           </div>
-          <h1 className="bg-gradient-to-r from-black to-stone-500 text-center md:text-left bg-clip-text font-display text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-6xl md:leading-[5rem]">
-            <Balancer>
-              I&apos;m Lance. I&apos;m a software developer.
-            </Balancer>
+          <h1 className="bg-gradient-to-br from-black to-stone-500 text-transparent text-center md:text-left bg-clip-text font-display text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]">
+            I&apos;m Lance. I&apos;m a software developer.
           </h1>
           <p className="max-w-[600px] mt-4 text-center md:text-left">
             On this site we explore different tools and technologies I
