7feb6ba9cff36494ac64b6f653103c646f6295b8
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-05-26 23:05:29 +0800

    add: resume w/ placeholder info

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,123 @@
+import Link from "next/link"
+import { ArrowUpRight, ExternalLink } from "lucide-react"
+
+interface ResumeInfo {
+  year: number
+  company: string
+  role: string
+  description: string[]
+}
+
+function SectionHeading({
+  left,
+  right,
+  link,
+}: {
+  left: string | number
+  right: string
+  link?: string
+}) {
+  return (
+    <div className="flex flex-row items-center gap-2 text-sm">
+      <span>{left}</span>
+      <hr className="h-[1px] w-full border-t-[1px] border-dotted border-black" />
+      {link ? (
+        <Link
+          className="group flex flex-row items-center gap-[1px] hover:underline"
+          href={link}
+          target="_blank"
+        >
+          {right}
+          <ArrowUpRight className="h-3 w-3" />
+        </Link>
+      ) : (
+        <span>{right}</span>
+      )}
+    </div>
+  )
+}
+
+function JobSection({ info }: { info?: ResumeInfo }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <SectionHeading left={2023} right={"Company"} />
+      <div className="flex flex-row items-start gap-10 text-sm">
+        <div className="flex">
+          <span className="whitespace-nowrap">Job Role</span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="text-justify text-black/80">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            rutrum, sapien sit amet volutpat lacinia, nisi ante gravida eros, a
+            volutpat tortor eros sed nisi.
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function Resume() {
-  return <main className="container mx-auto"></main>
+  return (
+    <main className="container mx-auto max-w-[690px]">
+      <div className="flex flex-col gap-4">
+        <span className="text-sm">LANCE AREVALO</span>
+        <div className="flex flex-row justify-between text-sm">
+          <span>SOFTWARE DEVELOPER</span>
+          <div className="flex flex-row items-center gap-2">
+            <span className="relative flex h-2 w-2">
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+              <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
+            </span>
+            <span>PHILIPPINES</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            Experience
+          </h4>
+          <JobSection />
+          <JobSection />
+          <JobSection />
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            Experience
+          </h4>
+          <SectionHeading
+            left={"Email"}
+            right={"hi@arevalolance.com"}
+            link="mailto:hi@arevalolance.com"
+          />
+          <SectionHeading
+            left={"LinkedIn"}
+            right={"arevalolance"}
+            link="https://linkedin.com/in/arevalolance"
+          />
+          <SectionHeading
+            left={"Read.cv"}
+            right={"arevalolance"}
+            link="https://read.cv/arevalolance"
+          />
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            About
+          </h4>
+          <p className="text-justify text-sm text-black/80">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            rutrum, sapien sit amet volutpat lacinia, nisi ante gravida eros, a
+            volutpat tortor eros sed nisi.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nullam rutrum, sapien sit amet volutpat
+            lacinia, nisi ante gravida eros, a volutpat tortor eros sed
+            nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            rutrum, sapien sit amet volutpat lacinia, nisi ante gravida eros, a
+            volutpat tortor eros sed nisi.
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
