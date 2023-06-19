f1013c9eb86a924fb53d74eaa7941e0c5bb5331b
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-06-19 18:57:19 +0800

    refactor: stylings on blog cards

--- components/blog-posts.tsx
+++ components/blog-posts.tsx
@@ -3,10 +3,17 @@
 import { useState } from "react"
 import Link from "next/link"
 import { allBlogs } from "@/.contentlayer/generated"
+import { ArrowUpRightIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
 import { Input } from "./ui/input"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip"
 
 const topics = ["Productivity", "Tools and Technology", "Thoughts", "Others"]
 
@@ -17,59 +24,68 @@ export default function BlogPosts() {
 
   return (
     <>
-      <section className="w-full lg:w-9/12">
-        <div className="mb-4">
+      <section className="w-full">
+        <div className="mb-4 flex max-w-[400px] flex-col gap-3">
           <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-            All posts by date
+            Writings
           </h3>
+          <p className="text-sm text-gray-500">
+            Here is where you&apos;ll find tutorials, life updates, and thoughts
+            on anything I find interesting.
+          </p>
         </div>
-        {allBlogs
-          .filter(
-            (e) =>
-              e.title.toLowerCase().includes(searchFilter) ||
-              e.summary.toLowerCase().includes(searchFilter) ||
-              e.body.raw.toLowerCase().includes(searchFilter)
-          )
-          .filter((e) => {
-            if (topicFilter !== "") {
-              return topicFilter === e.topic
-            } else {
-              return true
-            }
-          })
-          .sort((a, b) => {
-            if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-              return -1
-            }
-            return 1
-          })
-          .map((post) => (
-            <Link
-              key={post.slug}
-              className="group mb-4 flex flex-col space-y-1"
-              href={`/blog/${post.slug}`}
-            >
-              <div className="flex flex-col gap-2 rounded-md border-[1px] border-gray-300 p-3 duration-150 ease-in hover:shadow-md">
-                <div className="flex flex-col justify-between gap-2 lg:flex-row">
-                  <span className="font-semibold leading-none group-hover:underline">
-                    {post.title}
-                  </span>
-                  {post.topic && (
-                    <div className="flex flex-row items-start gap-2">
-                      <span className="whitespace-nowrap font-mono text-sm">
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3">
+          {allBlogs
+            .filter(
+              (e) =>
+                e.title.toLowerCase().includes(searchFilter) ||
+                e.summary.toLowerCase().includes(searchFilter) ||
+                e.body.raw.toLowerCase().includes(searchFilter)
+            )
+            .filter((e) => {
+              if (topicFilter !== "") {
+                return topicFilter === e.topic
+              } else {
+                return true
+              }
+            })
+            .sort((a, b) => {
+              if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+                return -1
+              }
+              return 1
+            })
+            .map((post) => (
+              <div className="group relative">
+                <TooltipProvider delayDuration={100}>
+                  <Tooltip>
+                    <TooltipTrigger className="absolute right-0 top-0 m-2 rounded-full border-[1px] border-transparent p-2 duration-150 ease-in group-hover:border-gray-200 group-hover:bg-white group-hover:drop-shadow-sm">
+                      <Link href={`/blog/${post.slug}`}>
+                        <ArrowUpRightIcon className="h-4 w-4" />
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent side="left">
+                      Read full blog post
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <div className="flex h-[300px] flex-col justify-between rounded-md bg-stone-50/50 p-4 duration-150 ease-in hover:bg-stone-100/50">
+                  <div className="text-sm text-gray-500">Writing · Blog</div>
+                  <div className="flex flex-col gap-2">
+                    <div className="grid grid-cols-1">
+                      <span className="text-xl font-medium">{post.title}</span>
+                      <span className="text-sm text-gray-500">
                         {post.publishedAt}
                       </span>
-                      <div className="h-fit w-fit whitespace-nowrap rounded-full bg-gray-100 px-3 py-1 text-xs font-semibold text-black ease-in hover:bg-gray-200 hover:duration-150 md:mx-0">
-                        {post.topic}
-                      </div>
                     </div>
-                  )}
+                    <span className="text-justify text-sm">{post.summary}</span>
+                  </div>
                 </div>
-                <p className="text-sm text-black/80">{post.summary}</p>
               </div>
-            </Link>
-          ))}
+            ))}
+        </div>
       </section>
+
       <section className="flex w-full flex-col gap-6 lg:w-3/12">
         <div className="flex flex-col gap-2">
           <div className="flex flex-row justify-between">
