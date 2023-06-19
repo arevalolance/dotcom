adb3469800d51753120ce0a992a2a3a7e4952973
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-06-19 14:56:04 +0800

    add: rss feed

---
+++ app/feed.xml/route.ts
@@ -0,0 +1,28 @@
+import { allBlogs } from "@/.contentlayer/generated"
+import RSS from "rss"
+
+export async function GET() {
+  const posts = allBlogs
+
+  const feed = new RSS({
+    title: "Lance Arevalo",
+    site_url: "https://arevalolance.com",
+    feed_url: "https://arevalolance.com/feed.xml",
+    language: "en",
+  })
+
+  posts.map((post) => {
+    feed.item({
+      title: post.title,
+      description: post.summary,
+      url: `https://arevalolance.com/blog/${post.slug}`,
+      date: post.publishedAt,
+    })
+  })
+
+  return new Response(feed.xml(), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  })
+}
