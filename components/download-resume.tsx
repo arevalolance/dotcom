5bce6182614909ec64ae42b9dae8db26b4a2c799
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-06-20 18:36:45 +0800

    refactor: disabled resume download button temporarily

--- components/download-resume.tsx
+++ components/download-resume.tsx
@@ -7,10 +7,11 @@ export default function DownloadResume() {
 
   return (
     <button
+      disabled
       onClick={() => router.push("/static/resume/arevalolance-resume.pdf")}
       className="border-gray-300/7 shadow-inner-[1px] rounded-md border-[1px] bg-gray-100 p-2  font-semibold drop-shadow-sm transition-colors duration-150 hover:border-black/20 hover:ease-in"
     >
-      Download PDF
+      Coming Soon
     </button>
   )
 }
