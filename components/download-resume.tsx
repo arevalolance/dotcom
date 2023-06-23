d52d654500c73edd437f4dadb81571e0975b0a2b
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-06-23 21:47:24 +0800

    add: updated resume (2023)

--- components/download-resume.tsx
+++ components/download-resume.tsx
@@ -7,8 +7,7 @@ export default function DownloadResume() {
 
   return (
     <button
-      disabled
-      onClick={() => router.push("/static/resume/arevalolance-resume.pdf")}
+      onClick={() => router.push("/static/resume/arevalolance-resume-2023.pdf")}
       className="border-gray-300/7 shadow-inner-[1px] rounded-md border-[1px] bg-gray-100 p-2  font-semibold drop-shadow-sm transition-colors duration-150 hover:border-black/20 hover:ease-in"
     >
       Coming Soon
